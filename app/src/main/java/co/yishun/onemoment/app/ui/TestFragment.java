8ff2539e9a7f8ddb0fbe16e3580744adaa4f59aa
a642500 <toxz@foxmail.com>
2015-07-13 20:41:47 +0800

    move package

--- app/src/main/java/co/yishun/onemoment/app/ui/TestFragment.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/TestFragment.java
@@ -1,10 +1,12 @@
-package co.yishun.onemoment.app;
+package co.yishun.onemoment.app.ui;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import co.yishun.onemoment.app.R;
 
 /**
  * A placeholder fragment containing a simple view.
