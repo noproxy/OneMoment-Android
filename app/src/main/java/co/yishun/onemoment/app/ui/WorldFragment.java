55b7a39180f64eaee5b8765a850f4ec667d9fe8a
a642500 <toxz@foxmail.com>
2015-07-14 13:52:15 +0800

    add viewpager and tab

--- app/src/main/java/co/yishun/onemoment/app/ui/WorldFragment.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/WorldFragment.java
@@ -1,6 +1,12 @@
 package co.yishun.onemoment.app.ui;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +23,47 @@ public final class WorldFragment extends BaseFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_world, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_world, container, false);
+        CoordinatorLayout coordinatorLayout = (CoordinatorLayout) rootView.findViewById(R.id.coordinatorLayout);
+        AppBarLayout appBar = (AppBarLayout) rootView.findViewById(R.id.appBar);
+        TabLayout tabLayout = (TabLayout) rootView.findViewById(R.id.tabLayout);
+        ViewPager viewPager = (ViewPager) rootView.findViewById(R.id.viewPager);
+
+        WorldViewPagerAdapter viewPagerAdapter = new WorldViewPagerAdapter(inflater.getContext());
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        return rootView;
+    }
+
+}
+
+class WorldViewPagerAdapter extends PagerAdapter {
+    private final Context context;
+
+    public WorldViewPagerAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override public Object instantiateItem(ViewGroup container, int position) {
+        boolean isRecommend = position == 0;
+        View rootView = LayoutInflater.from(context).inflate(R.layout.page_world, container,
+                false);
+        View worldSlider = rootView.findViewById(R.id.worldSlider);
+
+
+        if (isRecommend) {
+            worldSlider.setVisibility(View.VISIBLE);
+        } else {
+            worldSlider.setVisibility(View.INVISIBLE);
+        }
+        return rootView;
+    }
+
+    @Override public int getCount() {
+        return 2;
+    }
+
+    @Override public boolean isViewFromObject(View view, Object object) {
+        return false;
     }
 }
