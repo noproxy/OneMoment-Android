b5f0da2035b3ff878003318ab88ef333e4b7f9c0
a642500 <toxz@foxmail.com>
2015-07-20 18:54:24 +0800

    update discovery ui

---
+++ app/src/main/java/co/yishun/onemoment/app/ui/view/ObservableTwoWayView.java
@@ -0,0 +1,23 @@
+package co.yishun.onemoment.app.ui.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import org.lucasr.twowayview.widget.TwoWayView;
+
+/**
+ * Created by yyz on 7/20/15.
+ */
+public class ObservableTwoWayView extends TwoWayView {
+    public ObservableTwoWayView(Context context) {
+        super(context);
+    }
+
+    public ObservableTwoWayView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ObservableTwoWayView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
