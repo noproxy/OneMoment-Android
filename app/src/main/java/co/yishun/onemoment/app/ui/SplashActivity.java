3f1aaf976e882edfa922393d5fe1767d9c876505
a642500 <toxz@foxmail.com>
2015-07-23 17:23:34 +0800

    hide status bar for splash act

---
+++ app/src/main/java/co/yishun/onemoment/app/ui/SplashActivity.java
@@ -0,0 +1,71 @@
+package co.yishun.onemoment.app.ui;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.UiThread;
+import org.androidannotations.annotations.ViewById;
+import org.androidannotations.api.SdkVersionHelper;
+
+import co.yishun.onemoment.app.R;
+
+/**
+ * Created by yyz on 7/23/15.
+ */
+@EActivity(R.layout.activity_splash)
+public class SplashActivity extends AppCompatActivity {
+    public static final String RUNTIME_PREFERENCE = "run_preference";
+    public static final String PREFERENCE_IS_FIRST_LAUNCH = "is_first_launch";
+
+    @ViewById
+    ImageView splashImageView;
+
+    @SuppressLint("InlinedApi") @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (SdkVersionHelper.getSdkInt() >= 16) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startRecording();
+    }
+
+    @AfterViews void setResource() {
+        // do nothing because don't need different image
+//        boolean hasBackKey = KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_BACK);
+//        boolean hasHomeKey = KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_HOME);
+//
+//        if (hasBackKey && hasHomeKey) {
+//            // no navigation bar, unless it is enabled in the settings
+//            splashImageView.setImageResource(R.drawable.bg_welcome_no_nav);
+//        } else {
+//            // 99% sure there's a navigation bar
+//            splashImageView.setImageResource(R.drawable.bg_welcome);
+//        }
+    }
+
+    boolean isFirstLaunch() {
+        return getSharedPreferences(RUNTIME_PREFERENCE, MODE_PRIVATE).getBoolean(PREFERENCE_IS_FIRST_LAUNCH, true);
+    }
+
+
+    @UiThread(delay = 1600) void startRecording() {
+        this.finish();
+//        if (BuildConfig.DEBUG) new EveryDayNotification().onReceive(this, null);
+//        if (isFirstLaunch()) GuideActivity_.intent(this).start();
+//        else
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+}
