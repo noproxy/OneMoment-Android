484a279afd84290fbb4f0effaa5596a63d6585c7
a642500 <toxz@foxmail.com>
2015-07-24 23:14:03 +0800

    add login layout

---
+++ app/src/main/java/co/yishun/onemoment/app/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package co.yishun.onemoment.app.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+import co.yishun.onemoment.app.R;
+
+/**
+ * Created by yyz on 7/24/15.
+ */
+
+@EActivity(R.layout.activity_login)
+public class LoginActivity extends AppCompatActivity {
+
+}
