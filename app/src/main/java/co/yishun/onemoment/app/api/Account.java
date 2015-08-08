da35c71143b6bdb9cca0f363a51b42f645a2867e
Carlos <toxz@foxmail.com>
2015-08-08 18:03:01 +0800

    update account api

--- app/src/main/java/co/yishun/onemoment/app/api/Account.java
+++ app/src/main/java/co/yishun/onemoment/app/api/Account.java
@@ -3,7 +3,12 @@ package co.yishun.onemoment.app.api;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import java.util.List;
+
+import co.yishun.onemoment.app.api.model.ApiModel;
+import co.yishun.onemoment.app.api.model.Link;
 import co.yishun.onemoment.app.api.model.User;
+import co.yishun.onemoment.app.api.model.Video;
 import retrofit.http.Field;
 import retrofit.http.GET;
 import retrofit.http.POST;
@@ -14,55 +19,99 @@ import retrofit.http.Path;
  */
 public interface Account {
     @GET("/account/check_nickname")
-    boolean isNicknameExist(@NonNull @Field("nickname") String nickname);
+    ApiModel isNicknameExist(
+            @NonNull @Field("nickname") String nickname
+    );
 
     @POST("/account/signup")
-    void signUpByPhone(@Field("phone") @NonNull String phone,
-                       @Field("password") @NonNull String password);
-
-    @POST("/account/signin")
-    void signInByPhone(@Field("phone") @NonNull String phone,
-                       @Field("password") @NonNull String password);
-
-
-    @POST("/account/bind_weibo/{account_id}")
-    void bindWeibo(@NonNull @Path("account_id") String userId,
-                   @NonNull @Field("weibo_uid") String weiboUid);
-
-    @POST("/account/unbind_weibo/{account_id}")
-    void unbindWeibo(@NonNull @Path("account_id") String userId,
-                     @NonNull @Field("weibo_uid") String weiboUid);
-
+    User signUpByPhone(
+            @Field("phone") @NonNull String phone,
+            @Field("password") @NonNull String password);
 
     @POST("/account/weibo_signup")
-    void signUpByWeibo(@Field("uid") @NonNull String weiboUid,
-                       @Field("introduction") @Nullable String introduction,
-                       @Field("gender") @Nullable String gender,
-                       @Field("avatar_url") @Nullable String avatarUrl,
-                       @Field("location") @Nullable String location
+    User signUpByWeibo(
+            @Field("uid") @NonNull String weiboUid,
+            @Field("weibo_nickname") @NonNull String nickname,
+            @Field("gender") @NonNull String gender,
+            @Field("avatar_url") @NonNull String avatarUrl
+    );
+
+    @POST("/account/weixin_signup")
+    User signUpByWeixin(
+            @Field("uid") @NonNull String weixinUid,
+            @Field("weixin_nickname") @NonNull String nickname,
+            @Field("gender") @NonNull String gender,
+            @Field("avatar_url") @NonNull String avatarUrl
+    );
+
+    @POST("/account/qq_signup")
+    User signUpByQQ(
+            @Field("qq_number") @NonNull String qqNumber,
+            @Field("qq_nickname") @NonNull String nickname,
+            @Field("gender") @NonNull String gender,
+            @Field("avatar_url") @NonNull String avatarUrl
+    );
+
+    @POST("/account/signin")
+    User signInByPhone(
+            @Field("phone") @NonNull String phone,
+            @Field("password") @NonNull String password);
+
+    @POST("/account/update/{account_id}")
+    User updateInfo(
+            @Path("account_id") @NonNull String userId,
+            @Field("weibo_nickname") @Nullable String nickname,
+            @Field("gender") @Nullable String gender,
+            @Field("avatar_url") @Nullable String avatarUrl,
+            @Field("location") @Nullable String location
     );
 
     @POST("/account/send_verify_sms")
-    void sendVerifySms(@Field("phone") @NonNull String phone,
-                       @Field("String") @Nullable String signUpOrResetPassword);
+    ApiModel sendVerifySms(
+            @Field("phone") @NonNull String phone,
+            @Field("String") @Nullable String signUpOrResetPassword
+    );
 
     @POST("/account/verify_phone")
-    void verifyPhone(@Field("phone") @NonNull String phone,
-                     @Field("verify_code") @NonNull String verifyCode);
+    ApiModel verifyPhone(
+            @Field("phone") @NonNull String phone,
+            @Field("verify_code") @NonNull String verifyCode
+    );
 
     @GET("/account/account/{account_id}")
-    User getUserInfo(@Path("account_id") @NonNull String userId);
+    User getUserInfo(
+            @Path("account_id") @NonNull String userId
+    );
 
     @GET("/account/weibo/{weibo_id}")
-    void getUserInfoByWeiboUid(@Path("weibo_id") @NonNull String weiboUid);
+    User getUserInfoByWeiboUid(
+            @Path("weibo_id") @NonNull String weiboId
+    );
 
     @GET("/account/videos/{account_id}")
-    void getVideoList(@Path("account_id") @NonNull String userId);
+    List<Video> getVideoList(
+            @Path("account_id") @NonNull String userId
+    );
 
     @GET("/account/tag/download/{account_id}")
-    void getTagUrl(@Path("account_id") @NonNull String accountUid);
+    Link getTagUrl(
+            @Path("account_id") @NonNull String accountUid
+    );
 
     @POST("/account/reset_password")
-    void resetPassword(@Field("phone") @NonNull String phone, @Field("password") @NonNull String newPassword);
+    ApiModel resetPassword(
+            @Field("phone") @NonNull String phone,
+            @Field("password") @NonNull String newPassword
+    );
+
+//    @POST("/account/bind_weibo/{account_id}")
+//    void bindWeibo(
+//            @NonNull @Path("account_id") String userId,
+//            @NonNull @Field("weibo_uid") String weiboUid
+//    );
+//
+//    @POST("/account/unbind_weibo/{account_id}")
+//    void unbindWeibo(@NonNull @Path("account_id") String userId,
+//                     @NonNull @Field("weibo_uid") String weiboUid);
 }
 
