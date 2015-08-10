0b7eabf04982d1c9adaf61d6312da3c75595c25c
Carlos <toxz@foxmail.com>
2015-08-11 00:05:39 +0800

    remove toolbar

--- app/src/main/java/co/yishun/onemoment/app/ui/LoginActivity.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/LoginActivity.java
@@ -1,15 +1,10 @@
 package co.yishun.onemoment.app.ui;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 
-import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.ViewById;
 
 import co.yishun.onemoment.app.R;
 
@@ -21,18 +16,18 @@ import co.yishun.onemoment.app.R;
 public class LoginActivity extends AppCompatActivity {
     public static final int REQUEST_WEIBO_LOGIN = 0;
 
-    @ViewById
-    Toolbar toolbar;
-
-    @AfterViews void setToolbar() {
-        setSupportActionBar(toolbar);
-        final ActionBar ab = getSupportActionBar();
-        assert ab != null;
-
-        ab.setDisplayHomeAsUpEnabled(true);
-        ab.setDisplayShowTitleEnabled(false);
-        Log.i("setupToolbar", "set home as up true");
-    }
+//    @ViewById
+//    Toolbar toolbar;
+//
+//    @AfterViews void setToolbar() {
+//        setSupportActionBar(toolbar);
+//        final ActionBar ab = getSupportActionBar();
+//        assert ab != null;
+//
+//        ab.setDisplayHomeAsUpEnabled(true);
+//        ab.setDisplayShowTitleEnabled(false);
+//        Log.i("setupToolbar", "set home as up true");
+//    }
 
 
     @Click void loginByPhoneClicked(final View view) {
