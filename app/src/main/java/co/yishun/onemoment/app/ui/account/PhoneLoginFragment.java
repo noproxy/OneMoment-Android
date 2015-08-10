053750b508f365f87cb7eee3453ce95e58cc8b13
Carlos <toxz@foxmail.com>
2015-08-11 02:46:04 +0800

    small improve

--- app/src/main/java/co/yishun/onemoment/app/ui/account/PhoneLoginFragment.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/account/PhoneLoginFragment.java
@@ -31,34 +31,32 @@ public class PhoneLoginFragment extends PhonePasswordFragment {
 
     @Override
     public void onFABClick(View view) {
-        login();
+        if (checkPhoneNum() && checkPassword()) login();
     }
 
     @Background
     void login() {
-        if (checkPhoneNum() && checkPassword()) {
-            //TODO show progress bar
-            mActivity.showProgress(R.string.fragment_phone_login_login_progress);
-            User user = mActivity.getAccountService().signInByPhone(getPhoneNum(), getPassword());
-            mActivity.hideProgress();
-            if (user.code > 0) {
-                mActivity.showSnackMsg(R.string.fragment_phone_login_success);
-                //TODO add account and save info
-                exit();
-            } else switch (user.errorCode) {
-                case Constants.ErrorCode.PHONE_FORMAT_ERROR:
-                    mActivity.showSnackMsg(R.string.fragment_phone_login_phone_incorrect);
-                    break;
-                case Constants.ErrorCode.PASSWORD_NOT_CORRECT:
-                    mActivity.showSnackMsg(R.string.fragment_phone_login_error_password);
-                    break;
-                case Constants.ErrorCode.ACCOUNT_DOESNT_EXIST:
-                    mActivity.showSnackMsg(R.string.fragment_phone_login_error_account_not_exist);
-                    break;
-                default:
-                    mActivity.showSnackMsg(R.string.unknown_error);
-                    break;
-            }
+        //TODO show progress bar
+        mActivity.showProgress(R.string.fragment_phone_login_login_progress);
+        User user = mActivity.getAccountService().signInByPhone(getPhoneNum(), getPassword());
+        mActivity.hideProgress();
+        if (user.code > 0) {
+            mActivity.showSnackMsg(R.string.fragment_phone_login_success);
+            //TODO add account and save info
+            exit();
+        } else switch (user.errorCode) {
+            case Constants.ErrorCode.PHONE_FORMAT_ERROR:
+                mActivity.showSnackMsg(R.string.fragment_phone_login_phone_incorrect);
+                break;
+            case Constants.ErrorCode.PASSWORD_NOT_CORRECT:
+                mActivity.showSnackMsg(R.string.fragment_phone_login_error_password);
+                break;
+            case Constants.ErrorCode.ACCOUNT_DOESNT_EXIST:
+                mActivity.showSnackMsg(R.string.fragment_phone_login_error_account_not_exist);
+                break;
+            default:
+                mActivity.showSnackMsg(R.string.unknown_error);
+                break;
         }
     }
 
