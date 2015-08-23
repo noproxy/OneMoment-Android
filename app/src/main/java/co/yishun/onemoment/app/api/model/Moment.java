9e8a1b7404040d91dbb38f72d2462b1821eea461
Carlos <toxz@foxmail.com>
2015-08-23 09:28:36 +0800

    add parse method to Moment Model

--- app/src/main/java/co/yishun/onemoment/app/api/model/Moment.java
+++ app/src/main/java/co/yishun/onemoment/app/api/model/Moment.java
@@ -1,21 +1,56 @@
 package co.yishun.onemoment.app.api.model;
 
+import android.support.annotation.NonNull;
+
 import com.google.gson.annotations.SerializedName;
+import com.sina.weibo.sdk.utils.LogUtil;
+
+import co.yishun.onemoment.app.config.Constants;
 
 /**
  * Created by Carlos on 2015/8/8.
  */
-public class Moment extends ApiModel {
+public class Moment extends ApiModel implements Comparable<Moment> {
+    private static final String TAG = "Moment";
     public String mimeType = "video/mp4";
     public String hash;
     @SerializedName("fsize")
     public long fileSize;
-    public String key;
     public long putTime;
+    private String key;
 
+    //<userID>-<time>-<timestamp>.mp4
     // When be created by Gson into a List, auto set code 1
     public Moment() {
         this.code = 1;
         this.msg = "";
+    }
+
+    public String getTime() {
+        return this.key.substring(key.indexOf(Constants.URL_HYPHEN) + 1, key.lastIndexOf(Constants.URL_HYPHEN));
+    }
+
+    public String getUserID() {
+        String id = this.key.substring(0, key.indexOf(Constants.URL_HYPHEN));
+        LogUtil.v(TAG, "getUserID: " + id);
+        return id;
+    }
+
+    public long getTimeStamp() {
+        return Long.parseLong(key.substring(key.lastIndexOf(Constants.URL_HYPHEN) + 1, key.lastIndexOf(".")));
+    }
+
+
+    @Override
+    public String toString() {
+        return "Data{" +
+                "key='" + key + '\'' +
+                ", putTime='" + putTime + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(final @NonNull Moment moment) {
+        return (int) (putTime - moment.putTime);
     }
 }
