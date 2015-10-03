18e9d21ead9657395ee3b9e87edffbfb24c1816a
Carlos <toxz@foxmail.com>
2015-10-03 23:52:54 +0800

    add database compatible code

---
+++ app/src/main/java/co/yishun/onemoment/app/data/compat/Contract.java
@@ -0,0 +1,42 @@
+package co.yishun.onemoment.app.data.compat;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Contract for database and provider.
+ * <p>
+ * Created by Carlos on 3/9/15.
+ */
+public class Contract {
+
+    public static final String DATABASE_NAME = "OneDataBase.db";
+    public static final int DATABASE_VERSION = 2;//2.0 add "owner" column
+    public static final String AUTHORITY = "co.yishun.onemoment.app";
+
+
+    /**
+     * CompatMoment table info
+     */
+    public static class Moment implements BaseColumns {
+        public static final String TABLE_NAME = "moments";
+        public static final String CONTENT_URI_PATH = TABLE_NAME;
+
+        public static final String MIMETYPE_TYPE = TABLE_NAME;
+        public static final String MIMETYPE_NAME = AUTHORITY + "provider";
+
+        public static final String PATH = "path";
+        //TODO need??
+        public static final int CONTENT_URI_PATTERN_MANY = 1;
+        public static final int CONTENT_URI_PATTERN_ONE = 2;
+
+        public static final Uri CONTENT_URI = new Uri.Builder()
+                .scheme(ContentResolver.SCHEME_CONTENT)
+                .authority(AUTHORITY)
+                .appendPath(CONTENT_URI_PATH)
+                .build();
+
+    }
+
+}
---
+++ app/src/main/java/co/yishun/onemoment/app/data/compat/MomentDatabaseHelper.java
@@ -0,0 +1,77 @@
+package co.yishun.onemoment.app.data.compat;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import co.yishun.onemoment.app.data.model.Moment;
+import io.realm.Realm;
+import java8.util.stream.StreamSupport;
+
+/**
+ * SQLite opener helper for CompatMoment.
+ * <p>
+ * Created by Carlos on 3/9/15.
+ */
+public class MomentDatabaseHelper extends OrmLiteSqliteOpenHelper {
+
+    private static final String TAG = "MomentDatabaseHelper";
+    private final Context mContext;
+
+    public MomentDatabaseHelper(Context context) {
+        super(context, Contract.DATABASE_NAME, null, Contract.DATABASE_VERSION);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database, ConnectionSource connectionSource) {
+        // do nothing to use realm
+    }
+
+    /**
+     * own default value: LOCAL 's getPrefix(Context)
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase database, ConnectionSource connectionSource, int oldVersion, int newVersion) {
+        try {
+            Dao<CompatMoment, Integer> dao = getDao(CompatMoment.class);
+            Log.i(TAG, "upgrade database from " + oldVersion + " to " + newVersion);
+            switch (oldVersion) {
+                case 1:
+                    String renameTable = "ALTER TABLE `" + Contract.DATABASE_NAME + "` RENAME TO " + Contract.Moment.TABLE_NAME + " ;";//because I made wrong with naming table with database name.
+                    Log.i(TAG, "rename table: " + renameTable);
+                    dao.executeRaw(renameTable);
+                    String addColumn = "ALTER TABLE " + Contract.Moment.TABLE_NAME + " ADD COLUMN owner VARCHAR DEFAULT 'LOC' ;";
+                    Log.i(TAG, "add column: " + addColumn);
+                    dao.executeRaw(addColumn);
+                case 2:
+                    // switch to realm
+                    List<CompatMoment> allCompatMoments = dao.queryForAll();
+                    if (allCompatMoments.size() != 0) {
+                        Realm realm = Realm.getInstance(mContext);
+                        StreamSupport.stream(allCompatMoments).forEach(compatMoment -> {
+                            Moment moment = Moment.fromMomentProvider(compatMoment);
+                            realm.executeTransaction(realm1 -> realm1.copyToRealm(moment));
+                        });
+                    }
+                    TableUtils.dropTable(connectionSource, CompatMoment.class, true);
+                    break;
+                default:
+                    throw new IllegalStateException(
+                            "onUpgrade() with unknown oldVersion " + oldVersion);
+
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
