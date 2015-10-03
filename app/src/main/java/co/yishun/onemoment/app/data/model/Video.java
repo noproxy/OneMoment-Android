f2aa078d24f038e0c105a3194211e40b8b837515
Carlos <toxz@foxmail.com>
2015-10-03 15:55:59 +0800

    add Video Model

---
+++ app/src/main/java/co/yishun/onemoment/app/data/model/Video.java
@@ -0,0 +1,27 @@
+package co.yishun.onemoment.app.data.model;
+
+/**
+ * Created by Carlos on 2015/10/3.
+ */
+public class Video implements Moment.MomentProvider {
+
+
+    public Video(String path) {
+        //TODO implement
+    }
+
+    @Override
+    public String getTime() {
+        return null;
+    }
+
+    @Override
+    public String getOwnerID() {
+        return null;
+    }
+
+    @Override
+    public long getUnixTimeStamp() {
+        return 0;
+    }
+}
