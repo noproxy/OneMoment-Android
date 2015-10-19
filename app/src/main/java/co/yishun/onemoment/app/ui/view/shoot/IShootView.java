26594d5636861679cd3fae12bd1f85314e5238e4
Carlos <toxz@foxmail.com>
2015-10-19 12:26:13 +0800

    update IShootView interface

--- app/src/main/java/co/yishun/onemoment/app/ui/view/shoot/IShootView.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/view/shoot/IShootView.java
@@ -1,5 +1,10 @@
 package co.yishun.onemoment.app.ui.view.shoot;
 
+import java.io.File;
+
+import co.yishun.onemoment.app.function.Callback;
+import co.yishun.onemoment.app.function.Consumer;
+
 /**
  * Created by Carlos on 2015/10/9.
  */
@@ -15,4 +20,6 @@ public interface IShootView {
     boolean isFrontCameraAvailable();
 
     boolean isBackCamera();
+
+    void record(Callback recordStartCallback, Consumer<File> recordEndConsumer);
 }
