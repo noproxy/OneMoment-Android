f918ca4f377f842a69d283a5e8207385b5926217
Carlos <toxz@foxmail.com>
2015-10-29 23:16:59 +0800

    add Extra to get video recorded

--- app/src/main/java/co/yishun/onemoment/app/ui/MomentCreateActivity.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/MomentCreateActivity.java
@@ -2,8 +2,13 @@ package co.yishun.onemoment.app.ui;
 
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.VideoView;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Extra;
+import org.androidannotations.annotations.ViewById;
 
 import co.yishun.onemoment.app.R;
 import co.yishun.onemoment.app.ui.common.BaseActivity;
@@ -13,9 +18,25 @@ import co.yishun.onemoment.app.ui.common.BaseActivity;
  */
 @EActivity(R.layout.activity_moment_create)
 public class MomentCreateActivity extends BaseActivity {
+
+    @Extra String videoPath;
+    @ViewById VideoView videoView;
+
     @Nullable
     @Override
     public View getSnackbarAnchorWithView(@Nullable View view) {
         return null;
     }
+
+
+    @AfterViews
+    void setVideo() {
+        videoView.setVideoPath(videoPath);
+        videoView.start();
+    }
+
+    @Click
+    void nextBtnClicked(View view) {
+    }
+
 }
