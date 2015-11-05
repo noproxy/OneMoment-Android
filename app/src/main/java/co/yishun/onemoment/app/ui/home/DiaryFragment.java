54cf43c626e7b59198c9860eda0c9d0158fd48dc
Carlos <toxz@foxmail.com>
2015-11-05 20:01:11 +0800

    remove old test view in diary fragment

--- app/src/main/java/co/yishun/onemoment/app/ui/home/DiaryFragment.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/home/DiaryFragment.java
@@ -1,15 +1,10 @@
 package co.yishun.onemoment.app.ui.home;
 
 import android.support.v4.app.Fragment;
-import android.view.View;
-import android.widget.Toast;
 
-import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
-import org.androidannotations.annotations.ViewById;
 
 import co.yishun.onemoment.app.R;
-import co.yishun.onemoment.app.ui.view.CountDownView;
 
 /**
  * Created by yyz on 7/25/15.
@@ -17,17 +12,4 @@ import co.yishun.onemoment.app.ui.view.CountDownView;
 
 @EFragment(R.layout.fragment_diary)
 public class DiaryFragment extends Fragment {
-
-    @ViewById CountDownView countDownView;
-
-    @Click void btnClicked(View view) {
-        countDownView.setStartNumber(10);
-        countDownView.setOnCountDownEndListener(() -> {
-            Toast.makeText(view.getContext(), "End!", Toast.LENGTH_LONG).show();
-        });
-        countDownView.startCountDown();
-    }
-
-
-
 }
