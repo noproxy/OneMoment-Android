1d00be985fd48f78b2193b9e70cd27c55e4f0384
Carlos <toxz@foxmail.com>
2015-11-07 13:35:31 +0800

    add action menu

--- app/src/main/java/co/yishun/onemoment/app/ui/home/DiaryFragment.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/home/DiaryFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -36,6 +39,23 @@ public class DiaryFragment extends ToolbarFragment {
         View rootView = inflater.inflate(R.layout.fragment_diary, container, false);
         toolbar = (Toolbar) rootView.findViewById(R.id.toolbar);
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_fragment_diary, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.fragment_diary_action_share:
+                //TODO add share function
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
