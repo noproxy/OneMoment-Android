37b9524344929954cada4e172bf082f76e2b3922
Carlos <toxz@foxmail.com>
2015-11-09 19:37:56 +0800

    update database helper

--- app/src/main/java/co/yishun/onemoment/app/data/compat/MomentDatabaseHelper.java
+++ app/src/main/java/co/yishun/onemoment/app/data/compat/MomentDatabaseHelper.java
@@ -10,11 +10,8 @@ import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 
 import java.sql.SQLException;
-import java.util.List;
 
 import co.yishun.onemoment.app.data.model.Moment;
-import io.realm.Realm;
-import java8.util.stream.StreamSupport;
 
 /**
  * SQLite opener helper for CompatMoment.
@@ -24,16 +21,18 @@ import java8.util.stream.StreamSupport;
 public class MomentDatabaseHelper extends OrmLiteSqliteOpenHelper {
 
     private static final String TAG = "MomentDatabaseHelper";
-    private final Context mContext;
 
     public MomentDatabaseHelper(Context context) {
         super(context, Contract.DATABASE_NAME, null, Contract.DATABASE_VERSION);
-        mContext = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase database, ConnectionSource connectionSource) {
-        // do nothing to use realm
+        try {
+            TableUtils.createTableIfNotExists(connectionSource, Moment.class);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -46,23 +45,24 @@ public class MomentDatabaseHelper extends OrmLiteSqliteOpenHelper {
             Log.i(TAG, "upgrade database from " + oldVersion + " to " + newVersion);
             switch (oldVersion) {
                 case 1:
-                    String renameTable = "ALTER TABLE `" + Contract.DATABASE_NAME + "` RENAME TO " + Contract.Moment.TABLE_NAME + " ;";//because I made wrong with naming table with database name.
+                    String renameTable = "ALTER TABLE `" + Contract.DATABASE_NAME + "` RENAME TO " + Contract.Moment.TABLE_NAME + " ;";// because I made wrong with naming table with database name.
                     Log.i(TAG, "rename table: " + renameTable);
                     dao.executeRaw(renameTable);
                     String addColumn = "ALTER TABLE " + Contract.Moment.TABLE_NAME + " ADD COLUMN owner VARCHAR DEFAULT 'LOC' ;";
                     Log.i(TAG, "add column: " + addColumn);
                     dao.executeRaw(addColumn);
                 case 2:
-                    // switch to realm
-                    List<CompatMoment> allCompatMoments = dao.queryForAll();
-                    if (allCompatMoments.size() != 0) {
-                        Realm realm = Realm.getInstance(mContext);
-                        StreamSupport.stream(allCompatMoments).forEach(compatMoment -> {
-                            Moment moment = Moment.fromMomentProvider(compatMoment);
-                            realm.executeTransaction(realm1 -> realm1.copyToRealm(moment));
-                        });
-                    }
-                    TableUtils.dropTable(connectionSource, CompatMoment.class, true);
+                    // give up realm because lacking for multi-process support
+
+//                    List<CompatMoment> allCompatMoments = dao.queryForAll();
+//                    if (allCompatMoments.size() != 0) {
+//                        Realm realm = Realm.getInstance(mContext);
+//                        StreamSupport.stream(allCompatMoments).forEach(compatMoment -> {
+//                            Moment moment = Moment.fromMomentProvider(compatMoment);
+//                            realm.executeTransaction(realm1 -> realm1.copyToRealm(moment));
+//                        });
+//                    }
+//                    TableUtils.dropTable(connectionSource, CompatMoment.class, true);
                     break;
                 default:
                     throw new IllegalStateException(
