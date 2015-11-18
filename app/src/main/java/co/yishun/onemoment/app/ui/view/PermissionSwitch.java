e5fc92e71a70c31e38e44efb818efadd7aea1692
iamjinge <jiangjin863041@gmail.com>
2015-11-18 17:41:45 +0800

    fix switch

--- app/src/main/java/co/yishun/onemoment/app/ui/view/PermissionSwitch.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/view/PermissionSwitch.java
@@ -3,13 +3,16 @@ package co.yishun.onemoment.app.ui.view;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
+import android.support.v7.widget.SwitchCompat;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.ViewDebug;
+import android.view.WindowManager;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
-import android.widget.Switch;
 import android.widget.TextView;
 
 import co.yishun.onemoment.app.R;
@@ -18,11 +21,16 @@ import co.yishun.onemoment.app.R;
  * Created by Carlos on 2015/11/1.
  */
 public class PermissionSwitch extends LinearLayout {
+    private static final String TAG = "PermissionSwitch";
     TextView hintText;
-    Switch permissionSwitch;
+    SwitchCompat permissionSwitch;
+    TextView publicText;
+    TextView privateText;
     String onHintText;
     String offHintText;
-    // off == private, on == public
+    private int GRAY;
+    private int ORANGE;
+    // off == public, on == private
 
     public PermissionSwitch(Context context) {
         super(context);
@@ -49,12 +57,30 @@ public class PermissionSwitch extends LinearLayout {
         this.setOrientation(VERTICAL);
         this.setGravity(Gravity.CENTER);
         LayoutInflater.from(getContext()).inflate(R.layout.merge_permission_switch, this, true);
-        offHintText = getContext().getString(R.string.activity_moment_create_private_hint);
-        onHintText = getContext().getString(R.string.activity_moment_create_public_hint);
+        offHintText = getContext().getString(R.string.activity_moment_create_public_hint);
+        onHintText = getContext().getString(R.string.activity_moment_create_private_hint);
+        GRAY = getResources().getColor(R.color.colorGray);
+        ORANGE = getResources().getColor(R.color.colorOrange);
 
         hintText = (TextView) findViewById(R.id.hintText);
-        permissionSwitch = (Switch) findViewById(R.id.permissionSwitch);
+        permissionSwitch = (SwitchCompat) findViewById(R.id.permissionSwitch);
+        publicText = (TextView) findViewById(R.id.permissionPublicText);
+        privateText = (TextView) findViewById(R.id.permissionPrivateText);
+        publicText.setTextColor(ORANGE);
+        privateText.setTextColor(GRAY);
         hintText.setText(permissionSwitch.isChecked() ? onHintText : offHintText);
+
+        // for different screen size
+        DisplayMetrics metrics = new DisplayMetrics();
+        ((WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getMetrics(metrics);
+        Log.d(TAG, metrics.widthPixels + "  " + metrics.heightPixels);
+        if (metrics.widthPixels * 1f / metrics.heightPixels > 0.62f) {
+            hintText.setVisibility(GONE);
+        } else if (metrics.widthPixels * 1f / metrics.heightPixels > 0.6f) {
+            LayoutParams params = (LayoutParams) hintText.getLayoutParams();
+            params.topMargin /= 2;
+            hintText.setLayoutParams(params);
+        }
     }
 
     @ViewDebug.ExportedProperty
@@ -64,6 +90,8 @@ public class PermissionSwitch extends LinearLayout {
 
     public void setChecked(boolean checked) {
         permissionSwitch.setChecked(checked);
+        publicText.setTextColor(checked ? GRAY : ORANGE);
+        privateText.setTextColor(checked ? ORANGE : GRAY);
         hintText.setText(checked ? onHintText : offHintText);
     }
 
