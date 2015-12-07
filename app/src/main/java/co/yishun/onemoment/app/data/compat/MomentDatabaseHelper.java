022abc5fe455208982f6b91a8992e99b0afb64a1
Carlos <toxz@foxmail.com>
2015-12-07 20:20:12 +0800

    add table create code

--- app/src/main/java/co/yishun/onemoment/app/data/compat/MomentDatabaseHelper.java
+++ app/src/main/java/co/yishun/onemoment/app/data/compat/MomentDatabaseHelper.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
 
 import java.sql.SQLException;
 
@@ -20,16 +21,18 @@ import co.yishun.onemoment.app.data.model.Moment;
 public class MomentDatabaseHelper extends OrmLiteSqliteOpenHelper {
 
     private static final String TAG = "MomentDatabaseHelper";
-    private final Context mContext;
 
     public MomentDatabaseHelper(Context context) {
         super(context, Contract.DATABASE_NAME, null, Contract.DATABASE_VERSION);
-        mContext = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase database, ConnectionSource connectionSource) {
-        // do nothing to use realm
+        try {
+            TableUtils.createTableIfNotExists(connectionSource, Moment.class);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -48,23 +51,10 @@ public class MomentDatabaseHelper extends OrmLiteSqliteOpenHelper {
                     String addColumn = "ALTER TABLE " + Contract.Moment.TABLE_NAME + " ADD COLUMN owner VARCHAR DEFAULT 'LOC' ;";
                     Log.i(TAG, "add column: " + addColumn);
                     dao.executeRaw(addColumn);
-//                case 2:
-                    // switch to realm
-//                    List<CompatMoment> allCompatMoments = dao.queryForAll();
-//                    if (allCompatMoments.size() != 0) {
-//                        Realm realm = Realm.getInstance(mContext);
-//                        StreamSupport.stream(allCompatMoments).forEach(compatMoment -> {
-//                            Moment moment = Moment.fromMomentProvider(compatMoment);
-//                            realm.executeTransaction(realm1 -> realm1.copyToRealm(moment));
-//                        });
-//                    }
-//                    TableUtils.dropTable(connectionSource, CompatMoment.class, true);
                     break;
                 default:
                     throw new IllegalStateException(
                             "onUpgrade() with unknown oldVersion " + oldVersion);
-
-
             }
         } catch (SQLException e) {
             e.printStackTrace();
