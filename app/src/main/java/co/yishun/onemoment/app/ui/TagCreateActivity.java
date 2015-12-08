e51d0d504a623d59b3a61f07769cea12fcb47ef9
iamjinge <jiangjin863041@gmail.com>
2015-12-08 11:16:27 +0800

    change TagCreateActivity comments to english

--- app/src/main/java/co/yishun/onemoment/app/ui/TagCreateActivity.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/TagCreateActivity.java
@@ -257,9 +257,9 @@ public class TagCreateActivity extends BaseActivity implements AbstractRecyclerV
     void setupLocation() {
         locationClient = new LocationClient(getApplicationContext());
         LocationClientOption option = new LocationClientOption();
-        option.setLocationMode(LocationClientOption.LocationMode.Battery_Saving);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
-        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
-        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setLocationMode(LocationClientOption.LocationMode.Battery_Saving);
+        option.setCoorType("bd09ll");
+        option.setIsNeedAddress(true);//need address info
         option.setOpenGps(true);
         option.setIsNeedLocationDescribe(false);
         option.SetIgnoreCacheException(false);
