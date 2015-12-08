2e93c5ff7ad963cb40b6009e0cdd91a8e4f023e0
Carlos <toxz@foxmail.com>
2015-12-08 09:25:46 +0800

    add document comment

--- app/src/main/java/co/yishun/onemoment/app/api/model/QiniuKeyProvider.java
+++ app/src/main/java/co/yishun/onemoment/app/api/model/QiniuKeyProvider.java
@@ -1,6 +1,18 @@
 package co.yishun.onemoment.app.api.model;
 
 /**
+ * Interface to provide standard api naming. It must obey those naming rules:
+ *     <ul>
+ *     <li>
+ *         <strong>short video: </strong> {userID}-{time}-{timestamp}.mp4
+ *     </li>
+ *     <li>
+ *         <strong>long video: </strong> long-{userID}-{video_num}-{timestamp}.mp4
+ *     <li/>
+ *     </ul>
+ *
+ *
+ *
  * Created by Carlos on 2015/8/24.
  */
 public interface QiniuKeyProvider {
