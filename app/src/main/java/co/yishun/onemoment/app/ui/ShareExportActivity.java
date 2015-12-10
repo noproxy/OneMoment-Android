a5e8f55615f7be1118376b4ebb984f9ddbb2d0c1
iamjinge <jiangjin863041@gmail.com>
2015-12-10 23:09:03 +0800

    add ShareExportActivity

--- app/src/main/java/co/yishun/onemoment/app/ui/ShareExportActivity.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/ShareExportActivity.java
@@ -3,12 +3,17 @@ package co.yishun.onemoment.app.ui;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.j256.ormlite.dao.Dao;
 import com.squareup.picasso.Picasso;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.OrmLiteDao;
 import org.androidannotations.annotations.ViewById;
@@ -16,7 +21,9 @@ import org.androidannotations.annotations.ViewById;
 import java.io.File;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
 import co.yishun.library.calendarlibrary.DayView;
@@ -34,12 +41,27 @@ public class ShareExportActivity extends AppCompatActivity
     private static final String TAG = "ShareExportActivity";
     @ViewById Toolbar toolbar;
     @ViewById MomentCalendar momentCalendar;
+    @ViewById TextView shareText;
+    @ViewById TextView exportText;
+    @ViewById TextView selectAllText;
+    @ViewById TextView clearText;
+    @ViewById TextView selectedText;
 
     @OrmLiteDao(helper = MomentDatabaseHelper.class) Dao<Moment, Integer> momentDao;
+
+    private int totalMoments;
+    private List<Moment> selectedMoment;
 
     @AfterViews void setupViews() {
         momentCalendar.setAdapter(this);
         DayView.setOnMomentSelectedListener(this);
+        try {
+            totalMoments = (int) momentDao.queryBuilder().countOf();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        selectedMoment = new ArrayList<>();
+        updateSelectedText();
     }
 
     @AfterViews void setAppbar() {
@@ -49,7 +71,31 @@ public class ShareExportActivity extends AppCompatActivity
         ab.setDisplayHomeAsUpEnabled(true);
         ab.setTitle(R.string.activity_share_export_title);
         ab.setHomeAsUpIndicator(R.drawable.ic_action_back_close);
+    }
 
+    @Click(R.id.shareText) void shareTextClicked() {
+
+    }
+
+    @Click(R.id.exportText) void exportTextClicked() {
+
+    }
+
+    @Click(R.id.selectAllText) void selectAllTextClicked() {
+
+    }
+
+    @Click(R.id.clearText) void clearTextClicked() {
+
+    }
+
+    void updateSelectedText() {
+        String content = String.format(getResources().getString(R.string.activity_share_export_selected),
+                selectedMoment.size(), totalMoments);
+        SpannableString ss = new SpannableString(content);
+        ss.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorAccent)), 2,
+                content.indexOf("/"), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        selectedText.setText(ss);
     }
 
     @Override public void onBindView(Calendar calendar, DayView dayView) {
