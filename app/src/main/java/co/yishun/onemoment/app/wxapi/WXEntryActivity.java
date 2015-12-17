2bc94290550a457d3dd97101874f0edff0ba5f01
iamjinge <jiangjin863041@gmail.com>
2015-12-18 01:22:18 +0800

    fix weChat share feedback

--- app/src/main/java/co/yishun/onemoment/app/wxapi/WXEntryActivity.java
+++ app/src/main/java/co/yishun/onemoment/app/wxapi/WXEntryActivity.java
@@ -1,10 +1,12 @@
 package co.yishun.onemoment.app.wxapi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
 import co.yishun.onemoment.app.account.auth.WeChatHelper;
 import co.yishun.onemoment.app.ui.common.BaseActivity;
+import co.yishun.onemoment.app.ui.share.ShareActivity_;
 
 public class WXEntryActivity extends BaseActivity {
     private static final String TAG = "WXEntryActivity";
@@ -15,6 +17,12 @@ public class WXEntryActivity extends BaseActivity {
         Log.d("Share", "onCreate");
         if (EntryActivity.mAuthHelper != null && EntryActivity.mAuthHelper instanceof WeChatHelper)
             ((WeChatHelper) EntryActivity.mAuthHelper).handleIntent(getIntent());
+        else {
+            Intent intent = getIntent();
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.setClass(this, ShareActivity_.class);
+            startActivity(intent);
+        }
         finish();
     }
 
