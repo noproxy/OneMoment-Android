bb92cd96d3f19c2bc7bc82b77854328fbe10fecf
iamjinge <jiangjin863041@gmail.com>
2015-12-21 14:25:15 +0800

    guide layout

--- app/src/main/java/co/yishun/onemoment/app/ui/GuideActivity.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/GuideActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
@@ -16,22 +17,37 @@ import co.yishun.onemoment.app.ui.view.PageIndicatorDot;
 
 @EActivity(R.layout.activity_guide)
 public class GuideActivity extends AppCompatActivity {
+    private final int pagePicRes[] = new int[]{
+            R.drawable.pic_guide_001,
+            R.drawable.pic_guide_002,
+            R.drawable.pic_guide_003,
+            R.drawable.pic_guide_004};
+    private final int pageTextRes[] = new int[]{
+            R.drawable.pic_guide_001txt,
+            R.drawable.pic_guide_002txt,
+            R.drawable.pic_guide_003txt,
+            R.drawable.pic_guide_004txt};
+
     @ViewById ViewPager viewPager;
     @ViewById PageIndicatorDot pageIndicator;
-
     private int pageNum = 5;
 
     @AfterViews void setUpViews() {
         viewPager.setAdapter(getViewPager(LayoutInflater.from(this)));
-        viewPager.setOffscreenPageLimit(5);
+        viewPager.setOffscreenPageLimit(2);
         pageIndicator.setViewPager(viewPager);
         pageIndicator.setNum(pageNum);
     }
 
     protected View onCreatePagerView(LayoutInflater inflater, ViewGroup container, int position) {
-        View rootView = inflater.inflate(R.layout.page_guide, container, false);
-
-        container.addView(rootView);
+        View rootView;
+        if (position != 4) {
+            rootView = inflater.inflate(R.layout.page_guide, container, false);
+            ((ImageView)rootView.findViewById(R.id.picImage)).setImageResource(pagePicRes[position]);
+            ((ImageView)rootView.findViewById(R.id.textImage)).setImageResource(pageTextRes[position]);
+        } else {
+            rootView = inflater.inflate(R.layout.page_guide_final, container, false);
+        }
         return rootView;
     }
 
@@ -50,7 +66,6 @@ public class GuideActivity extends AppCompatActivity {
             @Override
             public Object instantiateItem(ViewGroup container, int position) {
                 View rootView = onCreatePagerView(inflater, container, position);
-
                 container.addView(rootView);
                 return rootView;
             }
