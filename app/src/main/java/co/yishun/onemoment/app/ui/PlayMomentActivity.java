5b0629fe810cecdaccd8cb1d72f8cc0f81a32e12
iamjinge <jiangjin863041@gmail.com>
2016-01-17 22:29:16 +0800

    remove share in PlayMomentActivity

--- app/src/main/java/co/yishun/onemoment/app/ui/PlayMomentActivity.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/PlayMomentActivity.java
@@ -2,60 +2,29 @@ package co.yishun.onemoment.app.ui;
 
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.FrameLayout;
 
-import com.afollestad.materialdialogs.MaterialDialog;
-import com.afollestad.materialdialogs.Theme;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.googlecode.mp4parser.authoring.Movie;
-import com.googlecode.mp4parser.authoring.Track;
-import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
-import com.googlecode.mp4parser.util.Matrix;
 import com.j256.ormlite.dao.Dao;
-import com.qiniu.android.storage.UploadManager;
 
 import org.androidannotations.annotations.AfterViews;
-import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Extra;
 import org.androidannotations.annotations.OrmLiteDao;
-import org.androidannotations.annotations.SupposeBackground;
-import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
 
-import java.io.File;
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
 
 import co.yishun.onemoment.app.LogUtil;
 import co.yishun.onemoment.app.R;
-import co.yishun.onemoment.app.Util;
 import co.yishun.onemoment.app.account.AccountManager;
-import co.yishun.onemoment.app.api.Account;
-import co.yishun.onemoment.app.api.Misc;
-import co.yishun.onemoment.app.api.authentication.OneMomentV3;
-import co.yishun.onemoment.app.api.model.ShareInfo;
-import co.yishun.onemoment.app.api.model.UploadToken;
-import co.yishun.onemoment.app.config.Constants;
-import co.yishun.onemoment.app.data.FileUtil;
 import co.yishun.onemoment.app.data.MomentLock;
 import co.yishun.onemoment.app.data.compat.MomentDatabaseHelper;
 import co.yishun.onemoment.app.data.model.Moment;
-import co.yishun.onemoment.app.data.model.OMLocalVideoTag;
-import co.yishun.onemoment.app.data.realm.RealmHelper;
 import co.yishun.onemoment.app.ui.common.BaseActivity;
 import co.yishun.onemoment.app.ui.play.PlayMomentFragment;
 import co.yishun.onemoment.app.ui.play.PlayMomentFragment_;
-import co.yishun.onemoment.app.video.VideoCommand;
-import co.yishun.onemoment.app.video.VideoConcat;
 
 @EActivity(R.layout.activity_play_moment)
 public class PlayMomentActivity extends BaseActivity {
@@ -71,10 +40,6 @@ public class PlayMomentActivity extends BaseActivity {
 
     private PlayMomentFragment playMomentFragment;
     private List<Moment> playingMoments;
-    private File videoCacheFile;
-    private MaterialDialog concatProgress;
-    private int totalTask = 1;
-    private int completeTask = 0;
 
     @AfterViews void setUpViews() {
         try {
