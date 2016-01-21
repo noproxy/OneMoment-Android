c4ecca306c03897bc2e4a856da6ed19bc3dbeb6c
iamjinge <jiangjin863041@gmail.com>
2016-01-21 22:40:59 +0800

    add web event handle

--- app/src/main/java/co/yishun/onemoment/app/ui/common/BaseWebActivity.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/common/BaseWebActivity.java
@@ -1,7 +1,30 @@
 package co.yishun.onemoment.app.ui.common;
 
+import android.support.annotation.CallSuper;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Extra;
+import org.androidannotations.annotations.ViewById;
+
 /**
  * Created by Jinge on 2016/1/21.
  */
+@EActivity
 public abstract class BaseWebActivity extends BaseActivity {
+
+    @Extra protected String title;
+    @Extra protected String url;
+
+    @ViewById protected Toolbar toolbar;
+
+    @CallSuper protected void setupToolbar() {
+        setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        assert ab != null;
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setDisplayShowTitleEnabled(true);
+        ab.setTitle(title);
+    }
 }
---
+++ app/src/main/java/co/yishun/onemoment/app/ui/common/CommonWebActivity.java
@@ -0,0 +1,21 @@
+package co.yishun.onemoment.app.ui.common;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+import co.yishun.onemoment.app.R;
+
+/**
+ * Created by Jinge on 2016/1/21.
+ */
+@EActivity(R.layout.activity_web_view)
+public class CommonWebActivity extends BaseWebActivity {
+
+    @AfterViews void setUpViews() {
+        setupToolbar();
+    }
+
+    @Override public void setPageInfo() {
+        mPageName = "CommonWebActivity";
+    }
+}
