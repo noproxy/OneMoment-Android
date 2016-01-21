8508d1d61832fbbaec05b4bebdbe54af3c2385e2
iamjinge <jiangjin863041@gmail.com>
2016-01-21 10:42:08 +0800

    add a model

---
+++ app/src/main/java/co/yishun/onemoment/app/api/model/World.java
@@ -0,0 +1,28 @@
+package co.yishun.onemoment.app.api.model;
+
+import java.util.List;
+
+/**
+ * Created by Jinge on 2016/1/20.
+ */
+public class World extends ApiModel {
+    public String _id;
+    public String accountId;
+    public String name;
+    public int updateTime;
+    public int createTime;
+    public int videosNum;
+    public int order;
+    public String type;
+    public boolean available;
+    public List<InvitedUser> invited;
+
+    public static class InvitedUser {
+        public String _id;
+        public boolean add;
+        public boolean read;
+        public boolean available;
+        public boolean accepted;
+        public int acceptTime;
+    }
+}
