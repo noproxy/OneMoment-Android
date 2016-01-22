bd72a64dadd33a4f73884e8330208d3767d66674
iamjinge <jiangjin863041@gmail.com>
2016-01-22 20:23:58 +0800

    add jump function

--- app/src/main/java/co/yishun/onemoment/app/ui/PersonalWorldActivity.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/PersonalWorldActivity.java
@@ -1,7 +1,34 @@
 package co.yishun.onemoment.app.ui;
 
+import org.androidannotations.annotations.AfterInject;
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+import java.io.File;
+
+import co.yishun.onemoment.app.R;
+import co.yishun.onemoment.app.config.Constants;
+import co.yishun.onemoment.app.data.FileUtil;
+import co.yishun.onemoment.app.ui.common.BaseWebActivity;
+
 /**
  * Created by Jinge on 2016/1/22.
  */
-public class PersonalWorldActivity {
+@EActivity(R.layout.activity_web_view)
+public class PersonalWorldActivity extends BaseWebActivity {
+
+    @AfterInject void setDefault() {
+        title = getString(R.string.app_name);
+        File hybrdDir = FileUtil.getInternalFile(this, Constants.HYBRD_UNZIP_DIR);
+        url = Constants.FILE_URL_PREFIX + new File(hybrdDir, "build/pages/add_to_world/add_to_world.html").getPath();
+    }
+
+    @AfterViews void setupViews() {
+        setupToolbar();
+        setupFragment();
+    }
+
+    @Override public void setPageInfo() {
+        mPageName = "PersonalWorldActivity";
+    }
 }
