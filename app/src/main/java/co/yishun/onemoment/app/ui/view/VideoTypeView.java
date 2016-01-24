05bfc5ed64f681506e4f4ae27a7d8cddcd32dfdb
iamjinge <jiangjin863041@gmail.com>
2016-01-24 15:21:36 +0800

    fix video type select

--- app/src/main/java/co/yishun/onemoment/app/ui/view/VideoTypeView.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/view/VideoTypeView.java
@@ -5,12 +5,14 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.PixelFormat;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.util.TypedValue;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -24,13 +26,17 @@ public class VideoTypeView extends RelativeLayout {
     private TextView mWorldTextView;
     private TextView mLifeTextView;
     private TextView mDiaryTextView;
+    private View mWorldClearView;
 
     private TextBgDrawable mWorldDrawable;
     private TextBgDrawable mLifeDrawable;
     private TextBgDrawable mDiaryDrawable;
+    private CloseDrawable mWorldClearDrawable;
 
     private int colorOrange;
     private int colorGray;
+    private int colorGrayDark;
+    private int colorWhite;
     private int oneDp;
 
     public VideoTypeView(Context context) {
@@ -61,17 +67,23 @@ public class VideoTypeView extends RelativeLayout {
         mWorldTextView = (TextView) findViewById(R.id.worldTextView);
         mLifeTextView = (TextView) findViewById(R.id.lifeTextView);
         mDiaryTextView = (TextView) findViewById(R.id.diaryTextView);
+        mWorldClearView = findViewById(R.id.worldClearView);
+        mWorldClearView.setVisibility(INVISIBLE);
 
         colorOrange = getResources().getColor(R.color.colorAccent);
-        colorGray = getResources().getColor(R.color.colorGray);
+        colorGray = getResources().getColor(R.color.colorVideoTypeGray);
+        colorGrayDark = getResources().getColor(R.color.colorVideoTypeGrayDark);
+        colorWhite = getResources().getColor(R.color.colorWhite);
 
-        mWorldDrawable = new TextBgDrawable(colorGray);
-        mLifeDrawable = new TextBgDrawable(colorGray);
-        mDiaryDrawable = new TextBgDrawable(colorGray);
+        mWorldDrawable = new TextBgDrawable(colorGrayDark, colorGray, true);
+        mLifeDrawable = new TextBgDrawable(colorGrayDark, colorGray);
+        mDiaryDrawable = new TextBgDrawable(colorGrayDark, colorGray);
+        mWorldClearDrawable = new CloseDrawable(colorWhite);
 
         mWorldTextView.setBackground(mWorldDrawable);
         mLifeTextView.setBackground(mLifeDrawable);
         mDiaryTextView.setBackground(mDiaryDrawable);
+        mWorldClearView.setBackground(mWorldClearDrawable);
 
         setWorldCheck(false, null);
         setLifeCheck(false);
@@ -82,31 +94,33 @@ public class VideoTypeView extends RelativeLayout {
         if (check) {
             mWorldTextView.setText(worldName);
             mWorldTextView.setTextColor(colorOrange);
-            mWorldDrawable.setColor(colorOrange);
+            mWorldDrawable.setColor(colorOrange, colorOrange);
+            mWorldClearView.setVisibility(VISIBLE);
         } else {
             mWorldTextView.setText(getResources().getString(R.string.video_type_add_to_world));
-            mWorldTextView.setTextColor(colorGray);
-            mWorldDrawable.setColor(colorGray);
+            mWorldTextView.setTextColor(colorGrayDark);
+            mWorldDrawable.setColor(colorGrayDark, colorGray);
+            mWorldClearView.setVisibility(INVISIBLE);
         }
     }
 
     public void setLifeCheck(boolean check) {
         if (check) {
             mLifeTextView.setTextColor(colorOrange);
-            mLifeDrawable.setColor(colorOrange);
+            mLifeDrawable.setColor(colorOrange, colorOrange);
         } else {
-            mLifeTextView.setTextColor(colorGray);
-            mLifeDrawable.setColor(colorGray);
+            mLifeTextView.setTextColor(colorGrayDark);
+            mLifeDrawable.setColor(colorGrayDark, colorGray);
         }
     }
 
     public void setDiaryCheck(boolean check) {
         if (check) {
             mDiaryTextView.setTextColor(colorOrange);
-            mDiaryDrawable.setColor(colorOrange);
+            mDiaryDrawable.setColor(colorOrange, colorOrange);
         } else {
-            mDiaryTextView.setTextColor(colorGray);
-            mDiaryDrawable.setColor(colorGray);
+            mDiaryTextView.setTextColor(colorGrayDark);
+            mDiaryDrawable.setColor(colorGrayDark, colorGray);
         }
     }
 
@@ -114,15 +128,15 @@ public class VideoTypeView extends RelativeLayout {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         LogUtil.d("size R", getWidth() + "  " + getHeight());
 
-        int textHeight = 0;
-        int margin = 0;
+        int textHeight;
+        int margin;
 
         if (getHeight() >= 92 * oneDp) {
             textHeight = 34 * oneDp;
             margin = 10 * oneDp;
         } else {
             textHeight = 28 * oneDp;
-            margin = (getHeight() - textHeight) / 3;
+            margin = (getHeight() - 2 * textHeight) / 3;
         }
 
         mWorldTextView.setHeight(textHeight);
@@ -130,52 +144,128 @@ public class VideoTypeView extends RelativeLayout {
         mDiaryTextView.setHeight(textHeight);
         ((LayoutParams) mWorldTextView.getLayoutParams()).bottomMargin = margin;
 
+        ((LayoutParams) mWorldClearView.getLayoutParams()).width = textHeight;
+
         mWorldDrawable.setSize(mWorldTextView.getWidth(), mWorldTextView.getHeight());
         mLifeDrawable.setSize(mLifeTextView.getWidth(), mLifeTextView.getHeight());
         mDiaryDrawable.setSize(mDiaryTextView.getWidth(), mDiaryTextView.getHeight());
+        mWorldClearDrawable.setSize(mWorldClearView.getWidth(), mWorldClearView.getHeight());
     }
 
     private class TextBgDrawable extends Drawable {
-        private Paint mPaint;
+        private Paint mStrokePaint;
+        private Paint mSolidPaint;
         private RectF mRect;
         private float mRadius;
+        private float mStrokeWidth;
+        private int mHeight;
+        private boolean mWithArrow;
+        private Path mPath;
 
-        public TextBgDrawable(int color) {
-            mPaint = new Paint();
-            mPaint.setStyle(Paint.Style.STROKE);
+        public TextBgDrawable(int strokeColor, int solidColor) {
+            this(strokeColor, solidColor, false);
+        }
 
-            mPaint.setAntiAlias(true);
-            mPaint.setColor(color);
-            mPaint.setStrokeWidth(oneDp);
+        public TextBgDrawable(int strokeColor, int solidColor, boolean withArrow) {
+            mStrokeWidth = oneDp * 1.3f;
+            mWithArrow = withArrow;
+            if (withArrow) mPath = new Path();
+
+            mStrokePaint = new Paint();
+            mStrokePaint.setStyle(Paint.Style.STROKE);
+            mStrokePaint.setAntiAlias(true);
+            mStrokePaint.setColor(strokeColor);
+            mStrokePaint.setStrokeWidth(mStrokeWidth);
+
+            mSolidPaint = new Paint();
+            mSolidPaint.setStyle(Paint.Style.FILL);
+            mSolidPaint.setAntiAlias(true);
+            mSolidPaint.setColor(solidColor);
+
             mRect = new RectF(0, 0, getWidth(), getHeight());
-            mRect.inset(oneDp / 2, oneDp / 2);
+            mRect.inset(mStrokeWidth / 2, mStrokeWidth / 2);
             LogUtil.d("size ", getWidth() + "  " + getHeight());
         }
 
-        public void setColor(int color) {
-            mPaint.setColor(color);
+        public void setColor(int strokeColor, int solidColor) {
+            mStrokePaint.setColor(strokeColor);
+            mSolidPaint.setColor(solidColor);
         }
 
         public void setSize(int width, int height) {
+            mHeight = height;
             mRect.set(0, 0, width, height);
-            mRect.inset(oneDp / 2, oneDp / 2);
+            mRect.inset(mStrokeWidth / 2, mStrokeWidth / 2);
             mRadius = height / 2;
+
+            if (mWithArrow) {
+                float dSize = 0.1667f;
+                mPath.reset();
+                mPath.moveTo(width - mRadius - dSize * height, (0.5f - dSize) * height);
+                mPath.lineTo(width - mRadius, height * 0.5f);
+                mPath.lineTo(width - mRadius - dSize * height, (0.5f + dSize) * height);
+            }
         }
 
         @Override public void draw(Canvas canvas) {
-            canvas.drawRoundRect(mRect, mRadius, mRadius, mPaint);
+            canvas.drawRoundRect(mRect, mRadius, mRadius, mStrokePaint);
+            canvas.drawCircle(mHeight / 2, mHeight / 2, mRadius / 2, mSolidPaint);
+            canvas.drawCircle(mHeight / 2, mHeight / 2, mRadius / 2, mStrokePaint);
+            if (mWithArrow)
+                canvas.drawPath(mPath, mStrokePaint);
         }
 
         @Override public void setAlpha(int alpha) {
-            mPaint.setAlpha(alpha);
+            mStrokePaint.setAlpha(alpha);
         }
 
         @Override public void setColorFilter(ColorFilter colorFilter) {
-            mPaint.setColorFilter(colorFilter);
+            mStrokePaint.setColorFilter(colorFilter);
         }
 
         @Override public int getOpacity() {
             return PixelFormat.TRANSLUCENT;
+        }
+    }
+
+    class CloseDrawable extends Drawable {
+        private Paint mPaint;
+        private float mWidth;
+        private float mHeight;
+        private float mStrokeWidth;
+
+        CloseDrawable(int color) {
+            mStrokeWidth = oneDp * 1.3f;
+            mPaint = new Paint();
+            mPaint.setStyle(Paint.Style.STROKE);
+            mPaint.setColor(color);
+            mPaint.setAntiAlias(true);
+            mPaint.setStrokeWidth(mStrokeWidth);
+        }
+
+        void setSize(float width, float height) {
+            mWidth = width;
+            mHeight = height;
+        }
+
+        @Override public void draw(Canvas canvas) {
+            float dSize = 0.5f - 1.414f / 8;
+            canvas.drawLine(dSize * mWidth, dSize * mHeight,
+                    (1 - dSize) * mWidth, (1 - dSize) * mHeight, mPaint);
+            canvas.drawLine((1 - dSize) * mWidth, dSize * mHeight,
+                    dSize * mWidth, (1 - dSize) * mHeight, mPaint);
+        }
+
+        @Override public void setAlpha(int alpha) {
+
+        }
+
+        @Override public void setColorFilter(ColorFilter colorFilter) {
+
+        }
+
+        @Override public int getOpacity() {
+            return 0;
         }
     }
 
