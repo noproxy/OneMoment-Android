0616c86dcc20413041be5d8d5f8dd83d4ae6ee57
iamjinge <jiangjin863041@gmail.com>
2016-01-26 15:06:38 +0800

    change banner url detail activity

--- app/src/main/java/co/yishun/onemoment/app/ui/hybrd/CommonWebActivity.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/hybrd/CommonWebActivity.java
@@ -1,5 +1,8 @@
 package co.yishun.onemoment.app.ui.hybrd;
 
+import android.text.TextUtils;
+
+import org.androidannotations.annotations.AfterInject;
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 
@@ -10,6 +13,10 @@ import co.yishun.onemoment.app.R;
  */
 @EActivity(R.layout.activity_tool_fragment)
 public class CommonWebActivity extends BaseWebActivity {
+
+    @AfterInject void setupDefault() {
+        if (TextUtils.isEmpty(title)) title = getString(R.string.app_name);
+    }
 
     @AfterViews void setupViews() {
         setupToolbar();
